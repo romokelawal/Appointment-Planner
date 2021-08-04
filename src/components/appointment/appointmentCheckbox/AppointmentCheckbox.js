db75a4d3fc05eec37c4e6dffb17f4a7c530becd2
Romoke Lawal <romokelawal08@gmail.com>
2021-08-04 20:23:26 +0200

    Completed title

--- src/components/appointment/appointmentCheckbox/AppointmentCheckbox.js
+++ src/components/appointment/appointmentCheckbox/AppointmentCheckbox.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 
 export const AppointmentCheckbox = ({ handleChecked }) => {
-
 	return (
 		<React.Fragment>
-				<td>
-					<Button id="appointmentChecked" onClick={handleChecked}></Button>
-				</td>
+			<td>
+				<Button id="appointmentChecked" onClick={handleChecked} />
+			</td>
 		</React.Fragment>
 	);
 };
